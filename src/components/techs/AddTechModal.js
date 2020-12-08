5cffbd01b230b7f288b2977ab93c6a98920849b9
phihuynh94 <phihuynh94@gmail.com>
2020-12-08 15:42:15 -0700

    add technians

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const AddTechModal = () => {
+import { addTech } from "../../actions/techActions";
+
+const AddTechModal = ({ addTech }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
@@ -9,7 +13,9 @@ const AddTechModal = () => {
     if (firstName === "" || lastName === "") {
       M.toast({ html: "Please enter a first and last name" });
     } else {
-      console.log(firstName, lastName);
+      addTech({ firstName, lastName });
+
+      M.toast({ html: `${firstName} ${lastName} was added as a tech` });
 
       // Clear Fields
       setFirstName("");
@@ -63,4 +69,8 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.prototype = {
+  addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
